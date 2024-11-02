9e6d9424d154b4d66d1176ae24ae338f73d0194b
Preston Hsiao <hsiaopreston97@gmail.com>
2024-11-02 10:51:00 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-import Hero from "./components/Hero";
+import Hero from "./components/hero";
 
 export default function Home() {
   return (
